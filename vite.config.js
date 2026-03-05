0d4a0052a760507664106d149d45195346b5df9d
Emerson Frasure <emersonfrasure@gmail.com>
2026-03-05 15:35:35 -0500

    Use repository's name as base

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
 
+const repoName = process.env.GITHUB_REPOSITORY?.split('/')[1]
+
 export default defineConfig({
-  base: '/catalog/',
+  base: repoName ? `/${repoName}/` : '/',
   plugins: [
     tailwindcss(),
   ],
